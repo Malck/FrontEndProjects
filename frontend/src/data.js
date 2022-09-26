fc6d8bd88275375688dff6ee9c89d5c9c1ca4822
MC <Malckira@gmail.com>
2022-09-26 15:31:51 +0200

    try to use a data file to store projects

---
+++ frontend/src/data.js
@@ -0,0 +1,33 @@
+import galaxie from "../src/assets/fond-galaxie.jpg"
+
+const data = [
+	{
+		"id": "1a",
+		"title": "NFT",
+        "url": "https://github.com/Malck",
+		"cover": "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-1.jpg",
+
+	},
+	{
+		"id": "2a",
+		"title": "Rating component",
+        "url": "https://github.com/Malck",
+		"cover": "${galaxie}",
+        
+	},
+	{
+		"id": "3a",
+		"title": "Studio de charme - Buttes Chaumont",
+		"cover": {galaxie},
+		
+		"description": "À seulement deux pas des Buttes Chaumont, venez découvrir Paris dans ce studio tout équipé. Entièrement équipé pour votre confort et élégamment décoré, il vous permettra de vivre comme un Parisien le temps de votre séjour.",
+		"host": {
+			"name": "Franck Maher",
+			"picture": "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/profile-picture-2.jpg"
+		},
+		
+	}
+	
+]
+
+export default data
