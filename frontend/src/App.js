0290aae41d61e1b3ecfd219d60d001cb5d561e05
MC <Malckira@gmail.com>
2022-09-26 15:28:33 +0200

    implement router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,9 @@
 import './App.css';
 import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router} from 'react-router-dom'
 
-import Home from "./pages/Home"; 
-//import Header from "../src/components/Header" ;
-import Error from "./pages/Error"
+
+import RouterPage from './router/RouterPage';
 
 
 
@@ -12,14 +11,7 @@ function App() {
   return (
     <Router>
   
-      <Routes>
-
-        <Route exact path="/" element={<Home />}/>
-
-
-        <Route path="/*" element={<Error />}/>
-          
-      </Routes>
+      <RouterPage/>
 
     </Router>
   );
