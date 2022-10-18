72e28d96d2d3c3bc4d39d24cb47a855d48221588
MC <Malckira@gmail.com>
2022-10-18 16:18:21 +0200

    minor changes

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,15 +5,15 @@ export default function Header() {
 
     <nav>
 
+        <div className="header-title"><a href="/" aria-current="page">Front-End Projects</a></div>
+
         <a href="/" className="nav-icon"
         aria-label="visit homepage"
         aria-current="page">
           <img src="finger.svg" alt="idea icon"/>
         </a>
 
-        <div className="header-title">Front-End Projects</div>
-
-
+    
         <div className="nav-authentification">
             
             <div className="navlinks-container">
--- frontend/src/router/RouterPage.jsx
+++ frontend/src/router/RouterPage.jsx
@@ -6,7 +6,7 @@ import Error from "../pages/Error.jsx"
 import Project from "../pages/Project"
 import Nft from "../creation/Nft_Card/Nft"
 import Rating from '../creation/Rating_component/Rating'
-import LocationPage from "../pages/Location"
+
 
 
 function RouterPage() {
@@ -21,10 +21,6 @@ function RouterPage() {
           <Route path="/nft" element={<Nft/>}/>
 
           <Route path="/rating" element={<Rating/>}/>
-
-
-          <Route  path='/locations/:id' element={<LocationPage />}/>
-          
 
           <Route path="/*" element={<Error />}/>
             
